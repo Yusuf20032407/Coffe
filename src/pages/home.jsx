422b6cb812d88bb606f0a36957b6ab3f8fbae045
Yusuf20032407 <Ylatipov007@gmail.com>
2025-09-28 04:23:05 +0500

    main qismi

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Card from '../components/card'
+import Mainqismi from '../components/main-qismi'
 
 
 const Home = () => {
     return (
         <div>
             <Card />
+            <Mainqismi />
         </div>
     )
 }
