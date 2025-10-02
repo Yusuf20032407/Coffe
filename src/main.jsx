bc6de4afed7324028795f93ff42386087980be74
Yusuf20032407 <Ylatipov007@gmail.com>
2025-10-03 00:02:43 +0500

    xato togrlandi

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { ConfigProvider } from "antd";
-import "leaflet/dist/leaflet.css";
 
 const root = createRoot(document.getElementById("root")); 
 root.render(
