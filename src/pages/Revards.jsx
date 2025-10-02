6eef23c6ef5364e7efc8ad1afbbb1fbe540ba748
Azam940 <azam0273755@gmail.com>
2025-10-02 21:43:14 +0500

    xato togillandi

--- src/pages/Revards.jsx
+++ src/pages/Revards.jsx
@@ -7,7 +7,7 @@ const Revards = () => {
       <div className='star bg-white'>
         <p className='montserrent text-[14px]'>Starbucks® Rewards</p>
       </div>
-      <div className='container mx-auto px-8 flex justify-between mb-[100px]'>
+      <div className='container mx-auto px-8 flex justify-between items-center mb-[100px]'>
         <div>
           <h1 className='montserrent text-[50px] text-white font-bold '>It's a great day for <br />free coffee</h1>
           <p className='montserrent text-[19px] text-white pt-[24px] pb-[34px]'>Sign up and start enjoying the perks of Starbucks® Rewards.</p>
