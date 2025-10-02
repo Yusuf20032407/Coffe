fb53e7c4ccff29f44b53596535d6b4841cd02cd4
Madaminbek <abduhoshimov1995@gmail.com>
2025-10-02 15:30:03 +0500

    Menu tabs qoshildi

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import React from "react";
+import { Tabs } from "antd";
 
 const Menu = () => {
   return (
     <div>
-  
+      <Tabs
+        color="white"
+        className="text-[#ffffffff]"
+        defaultActiveKey="1"
+        items={[
+          {
+            label: "Coffe",
+            key: "1",
+            children: "Tab 1",
+            className: "text-white",
+          },
+          {
+            label: "Tab 2",
+            key: "2",
+            children: "Tab 2",
+          },
+          {
+            label: "Tab 3",
+            key: "3",
+            children: "Tab 3",
+          },
+        ]}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
